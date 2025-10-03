b5e9b00b709dc8ea883d76043336c6a858bbb03f
Marcin Krzemiński <marcin@krzeminski.net>
2025-10-03 22:43:14 +0200

    fix: move toast notifications to bottom-right corner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Toaster position="top-right" richColors />
+        <Toaster position="bottom-right" richColors />
       </body>
     </html>
   );
